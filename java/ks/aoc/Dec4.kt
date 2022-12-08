a8b09b60a64657a0c5ef1213f28b3af597ffa36d
Karl Stjärne <karl.stjarne@cpacsystems.se>
2022-12-08 07:42:11 +0100

    Day 4 and 7
    Change-Id: I66a8c26d6c1e8c808a9652007e050d313fefdb37

---
+++ java/ks/aoc/Dec4.kt
@@ -0,0 +1,50 @@
+package ks.aoc
+
+object Dec4 {
+    private val REGEX = Regex("(-|,)")
+
+    fun a(): Int {
+        val input = FileUtil.loadResourceFile(javaClass.classLoader, "dec4")
+        var overlaps = 0
+
+        input.forEach {
+            val numbers = REGEX.split(it)
+            val e1Start = numbers[0].toInt()
+            val e1End = numbers[1].toInt()
+            val e2Start = numbers[2].toInt()
+            val e2End = numbers[3].toInt()
+            if ((e1Start in e2Start..e2End &&
+                        e1End in e2Start..e2End) ||
+                (e2Start in e1Start..e1End &&
+                        e2End in e1Start..e1End)
+            ) {
+                overlaps++
+            }
+        }
+
+        return overlaps
+    }
+
+
+    fun b(): Int {
+        val input = FileUtil.loadResourceFile(javaClass.classLoader, "dec4")
+        var overlaps = 0
+
+        input.forEach {
+            val numbers = REGEX.split(it)
+            val e1Start = numbers[0].toInt()
+            val e1End = numbers[1].toInt()
+            val e2Start = numbers[2].toInt()
+            val e2End = numbers[3].toInt()
+            if (e1Start in e2Start..e2End ||
+                e1End in e2Start..e2End ||
+                e2Start in e1Start..e1End ||
+                e2End in e1Start..e1End
+            ) {
+                overlaps++
+            }
+        }
+
+        return overlaps
+    }
+}
---
+++ java/ks/aoc/Dec7.kt
@@ -0,0 +1,87 @@
+package ks.aoc
+
+import java.util.*
+
+object Dec7 {
+
+    private const val DISK_SPACE = 70_000_000
+    private const val REQUIRED_SPACE = 30_000_000
+    private val fileStructure = hashMapOf<String, Node>()
+
+    fun a(): Int {
+        setup()
+        // Go through each node, find <= 100000
+        var totalSum = 0
+        fileStructure.forEach {
+            val node = it.value
+            if (node.type == Type.DIRECTORY && node.size <= 100_000) {
+                println("Node: ${node.id}, size: ${node.size}")
+                totalSum += node.size
+            }
+        }
+
+        return totalSum
+    }
+
+    fun b(): Int {
+        setup()
+        val usedSpace = fileStructure.asIterable().maxBy { it.value.size }.value.size
+        val missingSpace = REQUIRED_SPACE - (DISK_SPACE - usedSpace)
+        return fileStructure.asIterable()
+            .filter { it.value.type == Type.DIRECTORY }
+            .sortedBy { it.value.size }
+            .first { it.value.size >= missingSpace }
+            .value
+            .size
+    }
+
+    private fun setup() {
+        val input = FileUtil.loadResourceFile(javaClass.classLoader, "dec7")
+
+        var currentNode = Node(Type.DIRECTORY, "/", 0, "root-root", "")
+
+        // Build file structure
+        input.forEach {
+            if (it.startsWith("\$ cd ..")) {
+                currentNode = fileStructure[currentNode.parentId]!!
+            } else if (it.startsWith("\$ cd ")) {
+                val name = it.substring(5, it.length)
+                val id = UUID.randomUUID().toString()
+                currentNode = Node(
+                    Type.DIRECTORY,
+                    name,
+                    0,
+                    id,
+                    currentNode.id
+                )
+                fileStructure[id] = currentNode
+            } else if (!it.startsWith("\$") && !it.startsWith("dir")) {
+                val size = it.split(" ")[0].toInt()
+                val name = it.split(" ")[1]
+                val id = UUID.randomUUID().toString()
+                fileStructure[id] = Node(Type.FILE, name, size, id, currentNode.id)
+                currentNode.incSize(size)
+                incrementParentRecursive(currentNode.parentId, size)
+            }
+        }
+    }
+
+    private fun incrementParentRecursive(parentId: String, size: Int) {
+        if (parentId != "root-root") {
+            val parent: Node = fileStructure[parentId]!!
+            parent.incSize(size)
+            incrementParentRecursive(parent.parentId, size)
+        }
+    }
+
+    class Node(val type: Type, var name: String, var size: Int, val id: String, val parentId: String) {
+        fun incSize(sizeToAdd: Int) {
+            size += sizeToAdd
+        }
+    }
+
+    enum class Type {
+        DIRECTORY,
+        FILE
+    }
+}
