538e4201fa0d203f245ca134e0b50143c6b10e7a
Karl Stjärne <karl.stjarne@cpacsystems.se>
2022-12-09 08:09:44 +0100

    Day 8
    Change-Id: Ib3ddc60b6a0522fd035de907f095032da7107586

---
+++ java/ks/aoc/Dec8.kt
@@ -0,0 +1,117 @@
+package ks.aoc
+
+object Dec8 {
+
+    fun a(): Int {
+        val trees: List<List<Tree>> = getTreeMatrix()
+
+        trees.forEach {
+            updateTreeVisibility(it)
+            updateTreeVisibility(it.reversed())
+        }
+
+        transpose(trees).forEach {
+            updateTreeVisibility(it)
+            updateTreeVisibility(it.reversed())
+        }
+
+        return trees.sumOf { it.filter { tree -> tree.isSeen }.size }
+    }
+
+    fun b(): Int {
+        val trees: List<List<Tree>> = getTreeMatrix()
+
+        for (i in trees.indices) {
+            for (j in trees[i].indices) {
+                val tree = trees[i][j]
+
+                // Left
+                var k = j - 1
+                var scoreLeft = 0
+                while (trees[i].indices.contains(k)) {
+                    scoreLeft++
+                    if (tree.height <= trees[i][k].height) {
+                        break
+                    }
+                    k--
+                }
+
+                //Right
+                k = j + 1
+                var scoreRight = 0
+                while (trees[i].indices.contains(k)) {
+                    scoreRight++
+                    if (tree.height <= trees[i][k].height) {
+                        break
+                    }
+                    k++
+                }
+
+                // Up
+                k = i - 1
+                var scoreUp = 0
+                while (trees.indices.contains(k)) {
+                    scoreUp++
+                    if (tree.height <= trees[k][j].height) {
+                        break
+                    }
+                    k--
+                }
+
+                // Down
+                k = i + 1
+                var scoreDown = 0
+                while (trees.indices.contains(k)) {
+                    scoreDown++
+                    if (tree.height <= trees[k][j].height) {
+                        break
+                    }
+                    k++
+                }
+
+                tree.scenicScore = scoreLeft * scoreRight * scoreUp * scoreDown
+            }
+        }
+
+        return trees.maxOf { it.maxOf { tree -> tree.scenicScore } }
+    }
+
+    private fun getTreeMatrix(): List<List<Tree>> {
+        val input = FileUtil.loadResourceFile(javaClass.classLoader, "dec8")
+
+        val trees: List<List<Tree>> = List(input.size) { List(input.first().length) { Tree() } }
+        for (i in input.indices) {
+            for (j in input[i].indices) {
+                trees[i][j].height = input[i][j].digitToInt()
+            }
+        }
+        return trees
+    }
+
+    private fun updateTreeVisibility(trees: List<Tree>) {
+        var currentLargest = -1
+        for (i in trees.indices) {
+            val tree = trees[i]
+            if (tree.height > currentLargest) {
+                currentLargest = tree.height
+                tree.isSeen = true
+            }
+        }
+    }
+
+    private fun transpose(input: List<List<Tree>>): List<List<Tree>> {
+        val transpose: MutableList<MutableList<Tree>> = MutableList(input.size) { MutableList(input.first().size) { Tree() } }
+        for (i in input.indices) {
+            for (j in input[i].indices) {
+                transpose[j][i] = input[i][j]
+            }
+        }
+        return transpose
+    }
+
+    class Tree(
+        var isSeen: Boolean = false,
+        var height: Int = 0,
+        var scenicScore: Int = 0
+    )
+}
---
+++ java/ks/aoc/Tester.kt
@@ -0,0 +1,27 @@
+package ks.aoc
+
+import org.junit.Test
+
+class Tester {
+
+    @Test
+    fun test() {
+        println("Running tests")
+//        println("Dec 1a: ${Dec1.a()}")
+//        println("Dec 1b: ${Dec1.b()}")
+//        println("Dec 2a: ${Dec2.a()}")
+//        println("Dec 2b: ${Dec2.b()}")
+//        println("Dec 3a: ${Dec3.a()}")
+//        println("Dec 3b: ${Dec3.b()}")
+//        println("Dec 4a: ${Dec4.a()}")
+//        println("Dec 4b: ${Dec4.b()}")
+//        println("Dec 5a: ${Dec5.a()}")
+//        println("Dec 5b: ${Dec5.b()}")
+//        println("Dec 6a: ${Dec6.a()}")
+//        println("Dec 6b: ${Dec6.b()}")
+//        println("Dec 7a: ${Dec7.a()}")
+//        println("Dec 7b: ${Dec7.b()}")
+        println("Dec 8a: ${Dec8.a()}")
+        println("Dec 8b: ${Dec8.b()}")
+    }
+}
